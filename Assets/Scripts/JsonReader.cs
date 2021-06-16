0b68a11c05d3fd5bda65c15b8d026495da05d15d
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-06-16 16:02:05 +0530

    feat: read json data

---
+++ Assets/Scripts/JsonReader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReader : MonoBehaviour
+{
+    public TextAsset songDataJSON; //JSON File that conains the Song Details
+
+    [System.Serializable]
+    public class Song
+    {
+        public string name;
+        public string artist;
+        public string filename;
+    }
+
+    [System.Serializable]
+    public class SongList
+    {
+        public Song[] song;
+    }
+
+    public SongList mySongList = new SongList();
+
+    void Start()
+    {
+        mySongList = JsonUtility.FromJson<SongList>(songDataJSON.text);
+        foreach(Song song in mySongList.song)
+        {
+            Debug.Log(song.name);
+            Debug.Log(song.artist);
+            Debug.Log(song.filename);
+        }
+    }
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    
+
+    string currentRecord = "";
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            FindObjectOfType<AudioManager>().Stop(currentRecord);
+            currentRecord = "AAMusic";
+            FindObjectOfType<AudioManager>().Play("AAMusic");
+        }
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            FindObjectOfType<AudioManager>().Stop(currentRecord);
+            currentRecord = "NoWoman";
+            FindObjectOfType<AudioManager>().Play("NoWoman");
+        }
+        
+    }
+}
