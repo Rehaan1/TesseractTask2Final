ed36fb56da3d6f24ca5631561946242bbba688d7
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-06-17 17:06:36 +0530

    feat: added dancer

---
+++ Assets/Scripts/Dancer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class Dancer : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            PlayDancer();
+        }
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            StopDancer();
+        }
+    }
+    
+    public void PlayDancer()
+    {
+        playableDirector.Play();
+    }
+
+    public void StopDancer()
+    {
+        playableDirector.time=0;
+        playableDirector.Stop();
+        playableDirector.Evaluate();
+    }
+
+
+}
